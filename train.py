62e304c512e0ba27f0852f49a3922d68a80b6f50
unknown <1746387101@qq.com>
2022-05-26 21:05:53 +0800

    update

--- train.py
+++ train.py
@@ -1,36 +1,36 @@
+import os
 import torch
 import torchvision
 import matplotlib.pyplot as plt
 import torch.optim as optim
 import torch.nn.functional as F
 import argparse
+from torch.utils.tensorboard import SummaryWriter
+
+from utils.utils_data import get_params_groups, create_lr_scheduler
 from torch.utils.data import DataLoader
 from model import Net
 
 
-def load_mninst(args):
-
-    batch_size_train = 64
-    batch_size_test = 1000
-    random_seed = 1
+def load_mninst(batch_size_train, batch_size_test, random_seed):
     torch.manual_seed(random_seed)
 
     train_loader = torch.utils.data.DataLoader(
-      torchvision.datasets.MNIST('../data/', train=True, download=True,
-                                 transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                     (0.1307,), (0.3081,))
-                                 ])),
-      batch_size=batch_size_train, shuffle=True)
+        torchvision.datasets.MNIST('../data/', train=True, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=batch_size_train, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-      torchvision.datasets.MNIST('../data/', train=False, download=True,
-                                 transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                     (0.1307,), (0.3081,))
-                                 ])),
-      batch_size=batch_size_test, shuffle=True)
+        torchvision.datasets.MNIST('../data/', train=False, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=batch_size_test, shuffle=True)
 
     # examples = enumerate(test_loader)
     # batch_idx, (example_data, example_targets) = next(examples)
@@ -50,39 +50,125 @@ def load_mninst(args):
     return train_loader, test_loader
 
 
-def train(args, train_loader, test_loader, epoch):
+def train_one_epoch(model, optimizer, data_loader, device, epoch, lr_scheduler):
+
     train_losses = []
-    train_counter = []
-    test_losses = []
-    test_counter = [i * len(train_loader.dataset) for i in range(args.n_epochs + 1)]
-    network.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    loss_function = torch.nn.CrossEntropyLoss()
+    accu_num = torch.zeros(1).to(device)
+    sample_num = 0
+    model.train()
+    for batch_idx, (data, target) in enumerate(data_loader):
+        sample_num += data.shape[0]
         optimizer.zero_grad()
-        output = network(data)
-        loss = F.nll_loss(output, target)
+        output = model(data)
+        # loss = F.nll_loss(output, target)
+        output.data.max(1, keepdim=True)
+        pred = output.data.max(1, keepdim=True)[1]
+        loss = loss_function(pred, target.to(device))
+        accu_num += torch.eq(pred, target.to(device)).sum()
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
-            train_losses.append(loss.item())
-            train_counter.append(
-                (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset)))
-            torch.save(network.state_dict(), './model.pth')
-            torch.save(optimizer.state_dict(), './optimizer.pth')
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(data_loader.dataset),
+                    100. * batch_idx / len(data_loader), loss.item()))
+        train_losses.append(loss.item())
+
+    return train_losses, accu_num.item() / sample_num
 
 
+@torch.no_grad()
+def evaluate(model, data_loader, device, epoch):
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('n_epochs', type=int, default=3)
-    parser.add_argument('batch_size_train', type=int, default=64)
-    parser.add_argument('batch_size_test', type=int, default=1000)
-    parser.add_argument('log_interval', type=int, default=10)
-    parser.add_argument('random_seed', type=int, default=1)
-    learning_rate = 0.01
-    momentum = 0.5
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    model.eval()
+
+    accu_num = torch.zeros(1).to(device)   # 累计预测正确的样本数
+    accu_loss = torch.zeros(1).to(device)  # 累计损失
+
+    sample_num = 0
+
+    for step, data in enumerate(data_loader):
+        images, labels = data
+        sample_num += images.shape[0]
+
+        pred = model(images.to(device))
+        pred_classes = torch.max(pred, dim=1)[1]
+        accu_num += torch.eq(pred_classes, labels.to(device)).sum()
+
+        loss = loss_function(pred, labels.to(device))
+        accu_loss += loss
+
+        data_loader.desc = "[valid epoch {}] loss: {:.3f}, acc: {:.3f}, top_acc:{:.3f}".format(
+            epoch,
+            accu_loss.item() / (step + 1),
+            accu_num.item() / sample_num,
+        )
+
+    return accu_loss.item() / (step + 1), accu_num.item() / sample_num
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    print(f"using {device} device.")
+
+    if os.path.exists("./model") is False:
+        os.makedirs("./model")
+
+    train_loader, test_loader = load_mninst(args.batch_size_train, args.batch_size_test, args.random_seed)
+    model = Net().to(device)
+
+    # pg = get_params_groups(model, weight_decay=args.wd)
+    # optimizer = optim.AdamW(pg, lr=args.lr, weight_decay=args.wd)
+    # lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs,
+    #                                    warmup=True, warmup_epochs=1)
+
     network = Net()
-    optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-                          momentum=momentum)
+    optimizer = optim.SGD(network.parameters(), lr=args.learning_rate,
+                          momentum=args.momentum)
+
+    best_acc = 0.
+    tb_writer = SummaryWriter()
+    for epoch in range(args.epochs):
+        # train
+        train_loss, train_acc = train_one_epoch(model=model,
+                                                optimizer=optimizer,
+                                                data_loader=train_loader,
+                                                device=device,
+                                                epoch=epoch,
+                                                lr_scheduler=args.learning_rate)
+
+        # # validate
+        # val_loss, val_acc = evaluate(model=model,
+        #                              data_loader=val_loader,
+        #                              device=device,
+        #                              epoch=epoch)
+
+        # tags = ["train_loss", "train_acc", "val_loss", "val_acc", "learning_rate"]
+        # tb_writer.add_scalar(tags[0], train_loss, epoch)
+        # tb_writer.add_scalar(tags[1], train_acc, epoch)
+        # tb_writer.add_scalar(tags[2], val_loss, epoch)
+        # tb_writer.add_scalar(tags[3], val_acc, epoch)
+        # tb_writer.add_scalar(tags[4], optimizer.param_groups[0]["lr"], epoch)
+
+        if best_acc < train_acc:
+            # torch.save(model.state_dict(), "./weights/best_model.pth")
+            torch.save(model.state_dict(), './model/model.pth')
+            torch.save(optimizer.state_dict(), './model/optimizer.pth')
+            # best_acc = val_acc
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=3)
+    parser.add_argument('--batch_size_train', type=int, default=64)
+    parser.add_argument('--batch_size_test', type=int, default=1000)
+    parser.add_argument('--random_seed', type=int, default=1)
+    parser.add_argument('--log_interval', type=int, default=10)
+    parser.add_argument('--learning_rate', type=float, default=0.01)
+    parser.add_argument('--momentum', type=float, default=0.5)
+    parser.add_argument('--wd', type=float, default=5e-2)
+
+    opt = parser.parse_args()
+    main(opt)
+
--- utils/utils_data.py
+++ utils/utils_data.py
@@ -1,15 +1,13 @@
 import torch
 import torchvision
 import matplotlib.pyplot as plt
+import json
+import math
 from torch.utils.data import DataLoader
 
 def load_mninst():
-    n_epochs = 3
     batch_size_train = 64
     batch_size_test = 1000
-    learning_rate = 0.01
-    momentum = 0.5
-    log_interval = 10
     random_seed = 1
     torch.manual_seed(random_seed)
 
@@ -47,3 +45,56 @@ def load_mninst():
 
     return example_data, example_targets
 
+
+def get_params_groups(model: torch.nn.Module, weight_decay: float = 1e-5):
+    # 记录optimize要训练的权重参数
+    parameter_group_vars = {"decay": {"params": [], "weight_decay": weight_decay},
+                            "no_decay": {"params": [], "weight_decay": 0.}}
+
+    # 记录对应的权重名称
+    parameter_group_names = {"decay": {"params": [], "weight_decay": weight_decay},
+                             "no_decay": {"params": [], "weight_decay": 0.}}
+
+    for name, param in model.named_parameters():
+        if not param.requires_grad:
+            continue  # frozen weights
+
+        if len(param.shape) == 1 or name.endswith(".bias"):
+            group_name = "no_decay"
+        else:
+            group_name = "decay"
+
+        parameter_group_vars[group_name]["params"].append(param)
+        parameter_group_names[group_name]["params"].append(name)
+
+    print("Param groups = %s" % json.dumps(parameter_group_names, indent=2))
+    return list(parameter_group_vars.values())
+
+
+def create_lr_scheduler(optimizer,
+                        num_step: int,
+                        epochs: int,
+                        warmup=True,
+                        warmup_epochs=1,
+                        warmup_factor=1e-3,
+                        end_factor=1e-6):
+    assert num_step > 0 and epochs > 0
+    if warmup is False:
+        warmup_epochs = 0
+
+    def f(x):
+        """
+        根据step数返回一个学习率倍率因子，
+        注意在训练开始之前，pytorch会提前调用一次lr_scheduler.step()方法
+        """
+        if warmup is True and x <= (warmup_epochs * num_step):
+            alpha = float(x) / (warmup_epochs * num_step)
+            # warmup过程中lr倍率因子从warmup_factor -> 1
+            return warmup_factor * (1 - alpha) + alpha
+        else:
+            current_step = (x - warmup_epochs * num_step)
+            cosine_steps = (epochs - warmup_epochs) * num_step
+            # warmup后lr倍率因子从1 -> end_factor
+            return ((1 + math.cos(current_step * math.pi / cosine_steps)) / 2) * (1 - end_factor) + end_factor
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=f)
